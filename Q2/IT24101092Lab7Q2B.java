c675ae20b3acb4ded2d7650623d1cd0e41f9c43c
IT24101092 <it24101092@my.sliit.lk>
2024-09-25 00:08:06 +0530

    Add files via upload

---
+++ Q2/IT24101092Lab7Q2B.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ 
+public class IT24101092Lab7Q2B {
+       public static void main(String[] args) {
+
+       Scanner sr = new Scanner(System.in);
+
+       int i,j;
+
+       for(i=1; i<=5; i++)
+       {
+        System.out.print(+i + " - ");
+        for(j=1; j<=i; j++)
+        {
+         System.out.print("* ");
+        }
+        System.out.print("\n");
+       }
+     } 
+   }
